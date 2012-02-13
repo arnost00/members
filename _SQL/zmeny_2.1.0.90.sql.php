ccc49adafb64778db15adf5843492cef3dfb9509
Richard Patek <arnost00@users.noreply.github.com>
2012-02-13 15:45:29 +0000

    Pridano upozornovani na email pri snizeni zustatku pod stanovene meze - doplnen chybejici soubor pro pridani sloupcu do DB.

---
+++ _SQL/zmeny_2.1.0.90.sql.php
@@ -0,0 +1,24 @@
+<?
+
+//#############################################################################
+//	vychozi verze
+//#############################################################################
+
+$version_upd = '2.1.0.90';
+
+//#############################################################################
+
+require ('prepare.inc.php');
+
+//#############################################################################
+//	SQL dotazy pro zmenu db. na novejsi verzi
+//#############################################################################
+
+# *** zmena odkazu z accounts na users u tabulky finance
+$sql[1] = 'ALTER TABLE `'.TBL_MAILINFO.'` ADD `active_fin` TINYINT( 1 ) UNSIGNED NOT NULL , ADD `active_finf` TINYINT( 1 ) UNSIGNED NOT NULL , ADD `fin_type` INT( 11 ) UNSIGNED NOT NULL , ADD `fin_limit` SMALLINT( 5 ) NOT NULL ';
+//$sql[2] = '';
+
+//#############################################################################
+
+require ('action.inc.php');
+?>
