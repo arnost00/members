180c50b1c7e6e6d0569a8b85bb4fc49a6d99c7f4
kenia <9902994+xkenia@users.noreply.github.com>
2013-01-29 23:53:00 +0000

    added changes for table finance

--- _SQL/zmeny_2.1.0.91.sql.php
+++ _SQL/zmeny_2.1.0.91.sql.php
@@ -15,8 +15,14 @@ require ('prepare.inc.php');
 //#############################################################################
 
 # *** zmena nazvu sloupce z zavxus na id_zavod u tabulky finance
-$sql[1] = 'ALTER TABLE `'.TBL_FINANCE.'` CHANGE `id_zavxus` `id_zavod` INT( 10 ) UNSIGNED NULL DEFAULT NULL';
-//$sql[2] = '';
+$sql[1] = 'ALTER TABLE `'.TBL_FINANCE.'` CHANGE `id_zavxus` `id_zavod` INT( 10 ) unsigned DEFAULT NULL,
+`id_users_editor` smallint(5) unsigned NOT NULL,
+`id_users_user` smallint(5) unsigned NOT NULL,';
+
+$sql[2] = 'ALTER TABLE `'.TBL_FINANCE.'` ADD `storno` tinyint(1) DEFAULT NULL,
+`storno_by` int(10) unsigned DEFAULT NULL,
+`storno_date` date DEFAULT NULL,
+`storno_note` varchar(255) COLLATE cp1250_czech_cs DEFAULT NULL,';
 
 //#############################################################################
 
