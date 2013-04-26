0c405d7e6c5ac6c6d70c9a649c12b3febf3ced49
kenia <9902994+xkenia@users.noreply.github.com>
2013-04-26 12:47:28 +0000

    pridana moznost hromadneho zadani castky a poznamky pro platby pridani css class do radku pro table

--- race_finance.inc.php
+++ race_finance.inc.php
@@ -1,13 +1,10 @@
 <?php /* finance -  show exact race finance */
-?>
-
-<?
 
 $query_prihlaseni = "
 select u.id u_id, u.sort_name, f.id, f.amount, f.note, zu.kat from ".TBL_USER." u inner join
 ".TBL_ZAVXUS." zu on u.id = zu.id_user left join
 (select * from ".TBL_FINANCE." where id_zavod = $race_id and storno is null) f on f.id_users_user = zu.id_user
-where zu.id_zavod = $race_id order by u_id, id
+where zu.id_zavod = $race_id and u.hidden = '0' order by u.sort_name
 ";
 $vysledek_prihlaseni = mysql_query($query_prihlaseni);
 
@@ -15,8 +12,9 @@ $query_platici = "
 select u.id u_id, u.sort_name, f.id, f.amount, f.note, null kat from ".TBL_USER." u inner join
 (select * from ".TBL_FINANCE." where id_zavod = $race_id and storno is null) f on f.id_users_user = u.id
 where f.id_zavod = $race_id
-and u.id not in (select id_user from ".TBL_ZAVXUS." where id_zavod = $race_id)
-order by u_id, id
+and u.id not in (select id_user from ".TBL_ZAVXUS." where id_zavod = $race_id) 
+and u.hidden = '0' 
+order by u.sort_name
 ";
 $vysledek_platici = mysql_query($query_platici);
 
@@ -26,7 +24,9 @@ select u.id u_id, u.sort_name, null id, null amount, null note, null kat from ".
 FROM ".TBL_FINANCE." f where f.id_zavod = $race_id and f.storno is null
 union
 SELECT distinct(zu.id_user) id
-FROM ".TBL_ZAVXUS." zu where zu.id_zavod = $race_id) order by u_id, id
+FROM ".TBL_ZAVXUS." zu where zu.id_zavod = $race_id) 
+and u.hidden = '0' 
+order by u.sort_name
 ";
 $vysledek_neprihlaseni = mysql_query($query_neprihlaseni);
 
@@ -45,6 +45,53 @@ include_once ("./common_race.inc.php");
 RaceInfoTable($zaznam_z,'',false,false,true);
 
 include_once ('./url.inc.php');
+
+?>
+
+<style>
+div.update-categories {
+	margin: 10px 0 0 0;
+	padding: 10px 0 0 0;
+	border: 2px solid gray;
+	text-align: center;
+}
+
+div>div.sub-title {
+	text-align: left;
+}
+</style>
+
+<div class="update-categories">
+<div class="sub-title">Naplò pouze vybrané kategorie pro pøihlášené závodníky</div>
+Vše<input type="checkbox" id="all-ckbx"/><div id="ckbx-cat"></div>
+<label for="in-amount">Èástka</label><input type="number" id="in-amount"/>
+<label for="in-note">Poznámka</label><input type="text" id="in-note"/>
+<button onclick="fillInputsByCategory()">Vlož</button>
+</div>
+
+<script>
+//zaskrtnuti vse checkboxu po kliku na Vse
+$("#all-ckbx").click( function() {
+	$(".ckbx-cat").prop('checked', $("#all-ckbx").is(':checked'));
+});
+
+//naplni amount a note hodnotama z inputu in-amount a in-note
+function fillInputsByCategory() {
+	var kat = $("#in-kat").val();
+	var amount = $("#in-amount").val();
+	var note = $("#in-note").val();
+
+	jQuery.each( cats, function( index, value ) {
+		if ($('#ckbx-'+value).is(':checked')) {
+			$("input.amount-"+value).val(amount);
+			$("input.note-"+value).val(note);
+		}
+				
+	});
+}
+</script>
+
+<? 
 
 echo "<form method=\"post\" action=\"?payment=pay&race_id=$race_id\">";
 
@@ -74,19 +121,20 @@ while ($zaznam=mysql_fetch_assoc($vysledek_prihlaseni))
 	$row[] = $zaznam['sort_name'];
 	
 	$amount = $zaznam['amount'];
-	$input_amount = '<input type="number" id="am'.$i.'" name="am'.$i.'" value="'.$amount.'" size="5" maxlength="10" />';
+	$input_amount = '<input class="amount-'.$zaznam['kat'].'" type="number" id="am'.$i.'" name="am'.$i.'" value="'.$amount.'" size="5" maxlength="10" />';
 	$row[] = $input_amount;
 	
 	$note = $zaznam['note'];
-	$input_note = '<input type="text" id="nt'.$i.'" name="nt'.$i.'" value="'.$note.'" size="40" maxlength="200" />';
+	$input_note = '<input class="note-'.$zaznam['kat'].'" type="text" id="nt'.$i.'" name="nt'.$i.'" value="'.$note.'" size="40" maxlength="200" />';
 	$row[] = $input_note;
 	
-	$row[] = $zaznam['kat'];
+	$row[] = "<span class=\"cat\">".$zaznam['kat']."</span>";
 	$row_text = '<A HREF="javascript:open_win(\'./user_finance_view.php?user_id='.$zaznam['u_id'].'\',\'\')">Platby</A>';
 	$row_text .= '<input type="hidden" id="userid'.$i.'" name="userid'.$i.'" value="'.$zaznam["u_id"].'"/><input type="hidden" id="paymentid'.$i.'" name="paymentid'.$i.'" value="'.$zaznam["id"].'"/>'; 
 	$row[] = $row_text;
 	
-	echo $data_tbl->get_new_row_arr($row)."\n";
+	$row_class = "cat-".$zaznam['kat'];
+	echo $data_tbl->get_new_row_arr($row, $row_class)."\n";
 	$i++;
 }
 if ($i == 1)
@@ -182,3 +230,24 @@ echo $data_tbl->get_footer()."\n";
 <br>
 <input type="submit" value="Vytvoøit nové platby">
 </form>
+
+<script>
+//vlozeni checkboxu pro vyber kategorii, kterym se budou menit platby
+var ckbx = document.getElementById("ckbx-cat");
+var cats = [];
+$("span.cat").each(function(event) {
+	//vloz novou kategorii, pokud jiz neni v seznamu
+	if (cats.indexOf(this.innerHTML,0) < 0)	cats.push(this.innerHTML);
+});
+cats.sort();
+
+//vlozeni checkboxu do pripraveneho divu
+jQuery.each( cats, function( index, value ) {
+	ckbx.innerHTML += value+"<input type=\"checkbox\" class=\"ckbx-cat\" value=\"" + value + "\" id=\"ckbx-" + value + "\" /> ";
+});
+
+//pri kliku na jednu kategorii se odznaci Vse
+$("INPUT[type='checkbox'][class='ckbx-cat']").click( function() {
+	$("#all-ckbx").prop('checked', false); 
+});
+</script>
