f5e841ba62d3f31245a892d6bf5566bfd30096e2
kenia <kenia@4c8d6b59-980f-0410-8efe-cc4a2e0023c2>
2014-02-21 15:28:33 +0000

    Added total sum for whole club into finance directory list
    fixed http://mantis.eob.cz/view.php?id=66

---
+++ members/fin_directory_club_sum.php
@@ -0,0 +1,14 @@
+<HR>
+<table>
+<?
+$query_fin_club_sum = "select sum(if(f.amount<0,f.amount,0)) as minus, sum(if(f.amount>0,f.amount,0)) as plus, sum(f.amount) as total from ".TBL_FINANCE." f where (f.storno != 1 or f.storno is null);";
+
+@$result=MySQL_Query($query_fin_club_sum);
+
+$record=MySQL_Fetch_Array($result);
+
+echo "<tr><td><b>Total</b></td><td align=right><b>$record[total]</b></td></tr>";
+echo "<tr><td>Plus</td><td align=right>$record[plus]</td></tr><tr><td>Minus</td><td align=right>$record[minus]</td></tr>";
+
+?>
+</table>
