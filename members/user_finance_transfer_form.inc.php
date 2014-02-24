dcf8fbd1726df17a052933f8fb38b36b1d9a6ec7
kenia <kenia@4c8d6b59-980f-0410-8efe-cc4a2e0023c2>
2014-02-24 18:22:24 +0000

    not possible to transfer negative amount between members
    fixed http://mantis.eob.cz/view.php?id=67

--- members/user_finance_transfer_form.inc.php
+++ members/user_finance_transfer_form.inc.php
@@ -19,7 +19,7 @@ function createHTMLSelectFromSQLSelect($select_name, $option_value, $option_labe
 
 <hr>
 <h3>Transfer mezi èleny</h3>
-<form class="form" action="?<?=$return_url?>&payment=both" method="post">
+<form class="form" action="?<?=$return_url?>&payment=both" method="post" onsubmit="return isPositiveNumber(amount)">
 <?
 $data_tbl = new html_table_form();
 echo $data_tbl->get_css()."\n";
@@ -49,7 +49,7 @@ else
 
 echo $data_tbl->get_new_row('<label for="id_from">Pøevést od</label>', $from_select);
 echo $data_tbl->get_new_row('<label for="id_to">Pøevést komu</label>', $to_select);
-echo $data_tbl->get_new_row('<label for="amount">Èástka</label>', '<input name="amount" type="text" onkeyup="checkAmount(this);" size="5" maxlength="10" />');
+echo $data_tbl->get_new_row('<label for="amount">Èástka</label>', '<input name="amount" id="amount" type="text" size="5" maxlength="10" />');
 echo $data_tbl->get_new_row('<label for="note">Poznámka</label>', '<input name="note" type="text" size="40" maxlength="200" />');
 
 echo $data_tbl->get_empty_row();
