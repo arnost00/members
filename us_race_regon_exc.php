29883a1c818b363f22fce296465b7f3a73a40196
Richard Patek <arnost00@users.noreply.github.com>
2014-12-04 00:06:45 +0000

    Better handling when entry termin is 0.

--- us_race_regon_exc.php
+++ us_race_regon_exc.php
@@ -38,23 +38,26 @@ if ($kat != '')
 
 		$termin = raceterms::GetCurr4RegTerm($zaznam_z);
 
-		$transport = !isset($transport)? null: 1;
-
-		if ($novy)
+		if ($termin != 0) // not process if invalid termin number
 		{
-			$vysledek=MySQL_Query("SELECT * FROM ".TBL_ZAVXUS." WHERE id_zavod='$id_zav' and id_user='$id_us'");
-			if ($vysledek != FALSE && ($zaznam = MySQL_Fetch_Array($vysledek)) != FALSE )
-			{	// latest new == update
-				MySQL_Query("UPDATE ".TBL_ZAVXUS." SET kat='$kat', pozn='$pozn', pozn_in='$pozn2', termin='$termin', transport='$transport' WHERE id_zavod='$id_zav' and id_user='$id_us'");
+			$transport = !isset($transport)? null: 1;
+
+			if ($novy)
+			{
+				$vysledek=MySQL_Query("SELECT * FROM ".TBL_ZAVXUS." WHERE id_zavod='$id_zav' and id_user='$id_us'");
+				if ($vysledek != FALSE && ($zaznam = MySQL_Fetch_Array($vysledek)) != FALSE )
+				{	// latest new == update
+					MySQL_Query("UPDATE ".TBL_ZAVXUS." SET kat='$kat', pozn='$pozn', pozn_in='$pozn2', termin='$termin', transport='$transport' WHERE id_zavod='$id_zav' and id_user='$id_us'");
+				}
+				else
+				{	// really new
+					MySQL_Query("INSERT INTO ".TBL_ZAVXUS." (id_user, id_zavod, kat, pozn, pozn_in, termin, transport) VALUES ('$id_us','$id_zav','$kat','$pozn','$pozn2','$termin','$transport')");	
+				}
 			}
 			else
-			{	// really new
-				MySQL_Query("INSERT INTO ".TBL_ZAVXUS." (id_user, id_zavod, kat, pozn, pozn_in, termin, transport) VALUES ('$id_us','$id_zav','$kat','$pozn','$pozn2','$termin','$transport')");	
+			{	// update
+				MySQL_Query("UPDATE ".TBL_ZAVXUS." SET kat='$kat', pozn='$pozn', pozn_in='$pozn2', transport='$transport' WHERE id='$id_z'");
 			}
-		}
-		else
-		{	// update
-			MySQL_Query("UPDATE ".TBL_ZAVXUS." SET kat='$kat', pozn='$pozn', pozn_in='$pozn2', transport='$transport' WHERE id='$id_z'");
 		}
 	}
 }
