03529f7c97293c60d575871a8b7c96d7ae92c10c
Richard Patek <arnost00@users.noreply.github.com>
2015-02-17 11:49:09 +0000

    Oprava bugu 0000074, spravne zpracovani duplicit v kategoriich.

--- race_kat_exc.php
+++ race_kat_exc.php
@@ -17,32 +17,43 @@ $id = (IsSet($id) && is_numeric($id)) ? (int)$id : 0;
 
 db_Connect();
 
-$kat = '';
+$kat_arr = array();
+
 foreach ($kategorie_vypis as $kat_key => $kat_value)
 {
 	foreach ($zebricek_vypis as $zeb_key => $zeb_value)
 	{
 		if (IsSet($H[$kat_value][$zeb_value]) && $H[$kat_value][$zeb_value])
 		{
-			$kat=$kat.'H'.$kat_key.$zeb_key.';';
+			$kat_arr[] = 'H'.$kat_key.$zeb_key;
 		}
 		if (IsSet($D[$kat_value][$zeb_value]) && $D[$kat_value][$zeb_value])
 		{
-			$kat=$kat.'D'.$kat_key.$zeb_key.';';
+			$kat_arr[] ='D'.$kat_key.$zeb_key;
 		}
 	}
 	if (IsSet($H[$kat_value]['X']) && $H[$kat_value]['X'])
 	{
-		$kat=$kat.'H'.$kat_key.';';
+		$kat_arr[] ='H'.$kat_key;
 	}
 	if (IsSet($D[$kat_value]['X']) && $D[$kat_value]['X'])
 	{
-		$kat=$kat.'D'.$kat_key.';';
+		$kat_arr[] ='D'.$kat_key;
 	}
 
 }
 
-$kategorie = $kat.$kat_n;
+$kat_arr_n = explode(";",$kat_n);
+foreach ($kat_arr_n as $kat_n1)
+{
+	if (!in_array($kat_n1, $kat_arr) && $kat_n1 != '')
+	{
+		$kat_arr[] = $kat_n1;
+	}
+}
+
+
+$kategorie = implode(';',$kat_arr);
 $kategorie=correct_sql_string($kategorie);
 
 $result=MySQL_Query('UPDATE '.TBL_RACE." SET `kategorie`='$kategorie' WHERE `id`='$id'")
