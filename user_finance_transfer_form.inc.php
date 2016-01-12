05ebe12604b30b0272d52ca48fc2278e86b8a621
Richard Patek <arnost00@users.noreply.github.com>
2016-01-12 23:38:24 +0000

    Oprava bugu 0000079, kontrola castky pri zapisu platby, zruseno zbytne pole "Převést od". Rovnez vic kontrol pred zapisem platby.

--- user_finance_transfer_form.inc.php
+++ user_finance_transfer_form.inc.php
@@ -21,7 +21,6 @@ echo $data_tbl->get_header()."\n";
 
 $users_query = "select id, reg, sort_name  from `".TBL_USER."` where hidden=false order by sort_name asc;";
 $users_result=mysql_query($users_query);
-$from_options = array();
 $to_options = array();
 
 $opt = array();
@@ -34,13 +33,12 @@ while ($record=MySQL_Fetch_Array($users_result))
 	$opt = array();
 	$opt['value'] = $record['id'];
 	$opt['label'] = $record["sort_name"]." :: ".$record["reg"];
-	($record['id'] == $user_id)?$from_options[] = $opt:$to_options[] = $opt; 
+	if ($record['id'] != $user_id)
+		$to_options[] = $opt; 
 }
 
-$from_select = createHTMLSelect("id_from", $from_options);
 $to_select = createHTMLSelect("id_to", $to_options);
 
-echo $data_tbl->get_new_row('<label for="id_from">Převést od</label>', $from_select);
 echo $data_tbl->get_new_row('<label for="id_to">Převést komu</label>', $to_select);
 echo $data_tbl->get_new_row('<label for="amount">Částka</label>', '<input name="amount" id="amount" type="text" size="5" maxlength="10" />');
 echo $data_tbl->get_new_row('<label for="note">Poznámka</label>', '<input name="note" type="text" size="40" maxlength="200" />');
