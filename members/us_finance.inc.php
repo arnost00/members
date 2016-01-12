4355bbca901f1ad541095a3b73afc5e00e9b605f
arnost00 <arnost00@4c8d6b59-980f-0410-8efe-cc4a2e0023c2>
2016-01-12 23:38:24 +0000

    Oprava bugu 0000079, kontrola castky pri zapisu platby, zruseno zbytne pole "Převést od". Rovnez vic kontrol pred zapisem platby.

--- members/us_finance.inc.php
+++ members/us_finance.inc.php
@@ -12,9 +12,14 @@ $finance_readonly = "readonly";
 
 //---------- BLOK KODU PRO FINANCE ----------//
 require_once './payment.inc.php'; // pomocne funkce a javascript pro finance
-
-if (IsSet($payment) && IsSet($id_from) && IsSet($id_to) && IsSet($amount) && $id_to != -1)
+if (IsSet($payment) && IsSet($user_id) && IsSet($id_to) && IsSet($amount) && $id_to != -1)
 {
+	// set, clean & check values
+	$id_from = $user_id;
+	$amount = abs((int)$amount); // only positive numbers
+	$id_to = (int)$id_to;
+	$note = (IsSet($note)) ? correct_sql_string($note) : '';
+
 	//odecist penize z uctu ODKUD
 	createPayment($id_from, $id_from, -$amount, $note, null, null);
 	//pripsat penize na ucet KOMU
