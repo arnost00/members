331de344660b41667d8e9867b94edc33f2d13671
arnost00 <arnost00@4c8d6b59-980f-0410-8efe-cc4a2e0023c2>
2016-02-16 21:50:26 +0000

    Rozsireni exportu financi o datum a nazev zavodu.

--- members/fin_directory_export_exc.php
+++ members/fin_directory_export_exc.php
@@ -17,14 +17,14 @@ $delim = ';';
 $par2 = 1; //quotes
 $par3 = 0; //apostrophes
 
-$arr_labels = array('datum', 'reg.', 'jméno', 'částka', 'kometář');
-$arr_fields = array('date', 'reg', 'name', 'amount', 'note'); 
+$arr_labels = array('datum', 'reg.', 'jméno', 'částka', 'kometář', 'závod','datum závodu');
+$arr_fields = array('date', 'reg', 'name', 'amount', 'note','zavod_nazev','zavod_datum'); 
 
 TXT_Header();
 
 db_Connect();
 
-@$vysledek=MySQL_Query("SELECT f.date, concat('".$g_shortcut."',u.reg) as reg, u.sort_name as name, f.amount, f.note FROM `".TBL_FINANCE."` f join `".TBL_USER."` u on u.id = f.id_users_user where f.storno is null ORDER BY f.date desc")
+@$vysledek=MySQL_Query("SELECT f.date, concat('".$g_shortcut."',u.reg) as reg, u.sort_name as name, f.amount, f.note, rc.nazev zavod_nazev, from_unixtime(rc.datum,'%Y-%c-%e') zavod_datum FROM `".TBL_FINANCE."` f join `".TBL_USER."` u on u.id = f.id_users_user left join `".TBL_RACE."` rc on f.id_zavod = rc.id where f.storno is null ORDER BY f.date desc")
 	or die("Chyba při provádění dotazu do databáze.");
 
 require_once ('exports.inc.php');
