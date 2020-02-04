888edc22914a4525decd6d7a109cfa9eefb540f0
kenia <kenia@4c8d6b59-980f-0410-8efe-cc4a2e0023c2>
2020-02-04 14:50:36 +0000

    pri kopirovani tabulek pouze promazat, ne dropovat

--- members/_copy_production_tables.php
+++ members/_copy_production_tables.php
@@ -4,21 +4,24 @@
 /* ---- konfigurace ----
 */
 
-$g_dbserver='localhost';
-$g_dbuser='root';
-$g_dbpass='root';
-$g_dbname='members_rls';
+require ('./cfg/_cfg.php');
 
 //nastavit vsem heslo na '12345' - 1 = True, 0 = False
 $set_default_password=1;
 $default_password=md5('12345');
+$default_password_except = "'admin'";
+//nastavit vsem login stejny jako registracka
+//zatim nefunkcni, neni zadna rozumna logika, co tam dat
+$set_default_login=0;
+$default_login_except = "'admin', 'kenia', 'arnost'";
+
 
 //odkud se bude kopirovat
 $production_schema = 'zbm';
 //kam se bude kopirovat
-$copy_schema = 'copy_'.$production_schema;
+$copy_schema = 'tst2';
 //jake tabulky se budou kopirovat
-$tables_to_copy = ['zavod', 'news', 'users', 'accounts', 'usxus', 'zavxus', 'modify_log', 'mailinfo', 'finance', 'claim', 'finance_types'];
+$tables_to_copy = ['zavod', 'news', 'users', 'accounts', 'zavxus', 'modify_log', 'mailinfo', 'finance', 'claim', 'finance_types'];
 
 
 /*
@@ -39,21 +42,31 @@ foreach ($tables_to_copy as $table_name)
 	$source = $production_schema."_".$table_name;
 	$dest = $copy_schema."_".$table_name;
 	
-	//smazani tabulek
-	$query = "drop table if exists $dest";
+	//promazani tabulek
+	//dokud nebude pripraveno automaticke zakladani tabulek ze skriptu kvuli indexum apod., tak pouze promazavat
+	$query = "delete from $dest";
 	if ($mysqli->query($query) === TRUE) {
 		printf("Table $dest successfully deleted.<br/>");
 	} else 
 	echo "<b>$mysqli->error</b><br/>";
 	
-	//skpirovani tabulek
-	$query = "create table $dest as select * from $source";
+	//skopirovani dat z tabulek
+	$query = "INSERT INTO $dest SELECT * FROM $source";
 	if ($mysqli->query($query) === TRUE) {	
 		//nastavit vsem stejne defaultni heslo?
 		$password_updated = "";
+		$login_updates = "";
 		if ($table_name == 'accounts' and $set_default_password) {
-			$query = "update $dest set heslo = '".$default_password."'";
+			$query = "update $dest set heslo = '".$default_password."' where $dest.login NOT IN (".$default_password_except.")";
 			($mysqli->query($query) === TRUE)?$password_updated="and password was updated ":"";
+/*
+			//zatim nedoreseno, co tam dat jako defaultni login
+			if ($set_default_login)
+			{
+				$query = "UPDATE $dest a, `".$production_schema."_users` u SET a.login = u.reg WHERE a.id_users = u.id AND u.hidden = 0 AND a.login NOT IN (".$default_login_except.")";
+				echo $query;
+			}
+*/
 		}
 		printf("Table $source successfully copied ".$password_updated."to $dest table.<br/><br/>");
 	} else echo "<b>$mysqli->error</b><br/><br/>";
