bd9108c9d3c5d2a5c3cefdc856aab9c7c06c401c
kenia <kenia@4c8d6b59-980f-0410-8efe-cc4a2e0023c2>
2022-11-12 11:15:01 +0000

    Add db port to connection

--- members/_SQL/connect.inc.php
+++ members/_SQL/connect.inc.php
@@ -7,9 +7,9 @@ $db_conn = null;
 
 function db_connect ()
 {
-	global $g_dbserver,$g_dbuser,$g_dbpass,$g_dbname,$g_baseadr,$db_conn;
+	global $g_dbserver,$g_dbuser,$g_dbpass,$g_dbname,$g_dbport, $g_baseadr,$db_conn;
 
-	@$spojeni=new mysqli($g_dbserver,$g_dbuser,$g_dbpass) 
+	@$spojeni=new mysqli($g_dbserver,$g_dbuser,$g_dbpass,'',$g_dbport) 
 		or die ('Chyba pri pripojovani do db.');
 	$spojeni->select_db($g_dbname);
 	$spojeni->query("SET CHARACTER SET UTF8");
