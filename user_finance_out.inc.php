a2aa1e0958a5c9f8c5c5a4404f0f822d6c6599a1
Svatos Coufal <29300135+scoufal@users.noreply.github.com>
2025-10-14 17:39:20 +0200

    Preselect race on payement view
    For finance module update payment links to include race ID and enhance race selection with pre-selection option
    
    (cherry picked from commit 8ce2496e2aff9cf0155468755b67ed22f84f00f1)

--- user_finance_out.inc.php
+++ user_finance_out.inc.php
@@ -13,9 +13,10 @@ $race_sel .= '<select name="id_zavod">';
 $race_sel .= '<option value=null>---</option>';
 
 @$vysledek_zavody=query_db("select id, nazev, from_unixtime(datum,'%Y-%c-%e') datum_text from ".TBL_RACE." order by datum desc");
-while ($zaznam=mysqli_fetch_array($vysledek_zavody))
+while ($zaznam=mysqli_fetch_assoc($vysledek_zavody))
 {
-	$race_sel .= "<option value=".$zaznam["id"].">".$zaznam["nazev"]."&nbsp;-&nbsp;".formatDate($zaznam["datum_text"])."</option>";
+	$selected = (isset($race_id) && $zaznam["id"] == $race_id) ? ' selected' : '';
+	$race_sel .= "<option value=\"{$zaznam["id"]}\"{$selected}>".$zaznam["nazev"]."&nbsp;-&nbsp;".formatDate($zaznam["datum_text"])."</option>";
 }
 $race_sel .= '</select>&nbsp;&nbsp;nepovinná položka';
 
