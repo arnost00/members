967c5bb5307b584d3eb0ea548bff4240901a1b8d
Svatos Coufal <29300135+scoufal@users.noreply.github.com>
2026-01-10 22:55:23 +0100

    Count participans by PHP instead of SQL triggers

--- _SQL/zmeny_3.4.5.655.sql.php
+++ _SQL/zmeny_3.4.5.655.sql.php
@@ -22,12 +22,6 @@ $sql[3] = 'ALTER TABLE `'.TBL_RACE.'` ENGINE=InnoDB;';
 $sql[4] = 'UPDATE `'.TBL_RACE.'` r LEFT JOIN ( SELECT id_zavod, COUNT(*) AS cnt FROM `'.TBL_ZAVXUS.
 '` GROUP BY id_zavod ) z ON z.id_zavod = r.id SET r.prihlasenych = COALESCE(z.cnt, 0);';
 
-$sql[5] = 'CREATE TRIGGER `'.TBL_ZAVXUS.'_after_insert` AFTER INSERT ON `'.TBL_ZAVXUS.
-'`FOR EACH ROW UPDATE `'.TBL_RACE.'` SET prihlasenych = prihlasenych + 1 WHERE id = NEW.id_zavod;';
-
-$sql[6] = 'CREATE TRIGGER `'.TBL_ZAVXUS.'_after_delete` AFTER DELETE ON `'.TBL_ZAVXUS.
-'`FOR EACH ROW UPDATE `'.TBL_RACE.'` SET prihlasenych = prihlasenych - 1 WHERE id = OLD.id_zavod;';
-
 //#############################################################################
 
 require_once ('action.inc.php');
